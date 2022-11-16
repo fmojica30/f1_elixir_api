c43ae5ef49cd1df23156877fe32011f175d40291
Fernando Mojica <fernmarmojica@gmail.com>
2022-11-16 15:52:11 -0600

    Build initial files for API

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+const connectionString = process.env.DATABASE_URL || "";
+const connection = mysql.createConnection(connectionString);
+connection.connect();
+
+app.get("/api/drivers", (req, res) => {
+  const query = "select * from driver";
+  connection.query(query, (err, rows) => {
+    if (err) throw err;
+    const retVal = {
+      data: rows,
+      message: rows.length === 0 ? "No Records Found" : "Success",
+    };
+    return res.send(retVal);
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("App is running");
+});
